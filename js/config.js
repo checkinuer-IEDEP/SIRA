6416beb0ce702871d78ec7954e30ae30a1950635
checkinuer-IEDEP <checkin.uer@iedep.edu.mx>
2026-06-23 18:17:46 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   API_URL:
-  "https://script.google.com/macros/s/AKfycbxyvfy-Zoi3cgUqhINqwjD1Nl2AeAUXSXE3vv_kVVXEQ-FRoH7yzZG_vm7yckMZ5aD2/exec"
+  "https://script.google.com/macros/s/AKfycbxISCFqiHFg-yFAuCebfc2MtkqseWFl6_M3_gDk5jRwIosRXvubCFaFPNCGG_gK1MxzPQ/exec"
 };
