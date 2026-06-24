a6bc9409de2cb3a230dac63d494a4cc71f13097d
checkinuer-IEDEP <checkin.uer@iedep.edu.mx>
2026-06-23 18:17:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxyvfy-Zoi3cgUqhINqwjD1Nl2AeAUXSXE3vv_kVVXEQ-FRoH7yzZG_vm7yckMZ5aD2/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxISCFqiHFg-yFAuCebfc2MtkqseWFl6_M3_gDk5jRwIosRXvubCFaFPNCGG_gK1MxzPQ/exec";
 
 let deviceId = localStorage.getItem("deviceId");
 let selfieBase64 = null;
